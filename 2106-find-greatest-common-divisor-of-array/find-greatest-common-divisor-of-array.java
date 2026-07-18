28b322a41c2c26a1aee05cf968aa442ee03808e6
Satyaprakash Rout <99339162+SatyaPrakash252@users.noreply.github.com>
2026-07-18 10:50:46 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (28.11%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -1,15 +1,50 @@
-import java.math.BigInteger;
-class Solution {
-    public int findGCD(int[] nums) {
-        Arrays.sort(nums);
-        int n1 = nums[0];
-        int n2 = nums[nums.length-1];
-        BigInteger b1 = BigInteger.valueOf(n1);
-        BigInteger b2 = BigInteger.valueOf(n2);
+// import java.math.BigInteger;
+// class Solution {
+//     public int findGCD(int[] nums) {
+//         Arrays.sort(nums);
+//         int n1 = nums[0];
+//         int n2 = nums[nums.length-1];
+//         BigInteger b1 = BigInteger.valueOf(n1);
+//         BigInteger b2 = BigInteger.valueOf(n2);
         
-        BigInteger gcd = b1.gcd(b2);
+//         BigInteger gcd = b1.gcd(b2);
 
-        return gcd.intValue();
+//         return gcd.intValue();
 
+//     }
+// }
+
+class Solution {
+
+    public int findGCD(int[] nums) {
+
+        int min = nums[0];
+        int max = nums[0];
+
+        
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] < min) {
+                min = nums[i];
+            }
+
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+
+        return gcd(min, max);
+    }
+
+    public int gcd(int a, int b) {
+
+        while (b != 0) {
+
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
     }
 }
